b7a43983238d833a7193acd7b5156a818ddf29d7
Muammer Akca <muammerakca10@gmail.com>
2022-09-09 12:27:12 +0300

    Project Created and add UICollectionViewController and Cell's Class

---
+++ PhotoNoteApp/PersonCell.swift
@@ -0,0 +1,12 @@
+//
+//  PersonCell.swift
+//  PhotoNoteApp
+//
+//  Created by MAC on 9.09.2022.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+}
---
+++ PhotoNoteApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoNoteApp
+//
+//  Created by MAC on 9.09.2022.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .blue
+    }
+
+
+}
+
