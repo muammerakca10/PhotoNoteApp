c44847f1b3433b1e103d1f25324ff5d2e864c180
Muammer Akca <muammerakca10@gmail.com>
2022-09-09 14:58:38 +0300

    Fixing some problems

--- PhotoNoteApp/ViewController.swift
+++ PhotoNoteApp/ViewController.swift
@@ -41,12 +41,19 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         
         cell.imageView.image = UIImage(contentsOfFile: path.path)
         
+        cell.imageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
+            cell.imageView.layer.borderWidth = 2
+            cell.imageView.layer.cornerRadius = 3
+            cell.layer.cornerRadius = 7
+
+        
         return cell
     }
     
     func getDocumentsDirectory() -> URL{
         let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
+        let documentsDirectory = paths[0]
+        return documentsDirectory
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
@@ -61,10 +68,27 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
         
         let person = Person(name: "Unknown", image: imageName)
         people.append(person)
+        collectionView.reloadData()
         
         dismiss(animated: true)
-            
-        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let person = people[indexPath.item]
+
+        let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+
+        ac.addAction(UIAlertAction(title: "OK", style: .default) { [weak self, weak ac] _ in
+            guard let newName = ac?.textFields?[0].text else { return }
+            person.name = newName
+
+            self?.collectionView.reloadData()
+        })
+
+        present(ac, animated: true)
     }
     
     
