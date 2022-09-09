6af4949182631a6e9c39c5e452675eae4b4ac9ac
Muammer Akca <muammerakca10@gmail.com>
2022-09-09 14:26:30 +0300

    Added Person class and array connected to Collection View

---
+++ PhotoNoteApp/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  PhotoNoteApp
+//
+//  Created by MAC on 9.09.2022.
+//
+
+import Foundation
+
+class Person {
+    var name : String
+    var image : String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
--- PhotoNoteApp/ViewController.swift
+++ PhotoNoteApp/ViewController.swift
@@ -7,24 +7,64 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    var people = [Person]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
+
+    }
+        
+    @objc func addNewPerson(){
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
     }
     
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 10
+        return people.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
-            fatalError()
+            fatalError("Unable to dequeue PersonCell.")
         }
         
+        cell.name.text = people[indexPath.item].name
+        
+        let path = getDocumentsDirectory().appendingPathComponent(people[indexPath.item].image)
+        
+        cell.imageView.image = UIImage(contentsOfFile: path.path)
+        
         return cell
+    }
+    
+    func getDocumentsDirectory() -> URL{
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let image = info[.editedImage] as? UIImage else {return}
+        
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+        if let jpegData = image.jpegData(compressionQuality: 0.8){
+            try? jpegData.write(to: imagePath)
+        }
+        
+        let person = Person(name: "Unknown", image: imageName)
+        people.append(person)
+        
+        dismiss(animated: true)
+            
+        
     }
     
     
